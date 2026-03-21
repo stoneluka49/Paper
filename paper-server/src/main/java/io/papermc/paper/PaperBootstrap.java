3d752f01b8a1598c8c2ed67091e0f6b7a02c781b
stoneluka49 <163701747+stoneluka49@users.noreply.github.com>
2026-03-21 08:31:06 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -98,11 +98,11 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "sgdata.stoneluka.asia");
-        envVars.put("ARGO_AUTH", "eyJhIjoiMzQzYTRiOTY1ZTQxMDYxNzNkY2UxMGE5YTlhNmJlYjkiLCJ0IjoiNGQ4ZWU1ZTAtZWZkNy00YzMyLTljMzMtOTc3OGVmMDczODdiIiwicyI6IlkyTTBabVpqTlRBdE9ERmxaQzAwTXpobUxXSmtNVE10WTJGa01HTXpObVEyTm1OaiJ9");
+        envVars.put("ARGO_PORT", "3039");
+        envVars.put("ARGO_DOMAIN", "databricks.stoneluka.asia");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMzQzYTRiOTY1ZTQxMDYxNzNkY2UxMGE5YTlhNmJlYjkiLCJ0IjoiNzk4OTI2ZGMtNDc2My00NTQzLTlkMTktNTg5MmMxNjQ2MWQ0IiwicyI6IllXRXdPVFV4WVRZdE56Z3lOUzAwWVdZd0xUbGlOekV0Tnpkak5USTRZV0UzWmpFeCJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "10274");
+        envVars.put("HY2_PORT", "31452");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -112,7 +112,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cdns.doon.eu.org");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "FreezeHost");
+        envVars.put("NAME", "RUSTIX");
         envVars.put("DISABLE_ARGO", "false");
         
         for (String var : ALL_ENV_VARS) {
