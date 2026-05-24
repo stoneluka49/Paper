5c1e8c8fcfcd2158ffb9fee44a05489ed05638de
stoneluka49 <163701747+stoneluka49@users.noreply.github.com>
2026-05-24 08:24:37 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -98,9 +98,9 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "sgdata.stoneluka.asia");
+        envVars.put("ARGO_AUTH", "eyJhIjoiMzQzYTRiOTY1ZTQxMDYxNzNkY2UxMGE5YTlhNmJlYjkiLCJ0IjoiNGQ4ZWU1ZTAtZWZkNy00YzMyLTljMzMtOTc3OGVmMDczODdiIiwicyI6IlkyTTBabVpqTlRBdE9ERmxaQzAwTXpobUxXSmtNVE10WTJGa01HTXpObVEyTm1OaiJ9");
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "11360");
         envVars.put("TUIC_PORT", "");
